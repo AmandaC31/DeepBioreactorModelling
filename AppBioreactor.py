fcef52bb2f333396731a0b7ef9e77d1317b287f6
Amanda <amandacreche2001@gmail.com>
2022-11-27 12:42:37 -0500

    C and P 2

--- AppBioreactor.py
+++ AppBioreactor.py
@@ -32,21 +32,26 @@ st.sidebar.header('User Input file')
 uploaded_file = st.sidebar.file_uploader("Upload your CSV file with recorded data to display them and obtain your feedstock characteristics", type=["csv"])
 if uploaded_file is not None:
    input_df = pd.read_csv(uploaded_file)
-#elif not(example_number == 1234):
 #   input_df = Example().example_data[example_number-1]
 else:
-    st.write("Select an example file to try:")
-    example = st.sidebar.selectbox(label='Use Example',
-                                   options=(  # '<None selected>',
+    example = st.sidebar.selectbox(label='Or select an example file',
+                                   options=(
                                        'Example 1', 'Example 2', 'Example 3'))
+    if 'Example 1' in example:
+        input_df=pd.read_csv("example1.csv")
+    elif 'Example 2' in example:
+        input_df=pd.read_csv("example2.csv")
+    elif 'Example 3' in example:
+        input_df=pd.read_csv("example3.csv")
     # try:
-    example_number = int(example.split(" ")[1])
-    prepared = Example()
-    st.sidebar.write(prepared.disp_example(example_number))
-    input_df = prepared.display_data[example_number]
+    #example_number = int(example.split(" ")[1])
+    #prepared = Example()
+    #st.sidebar.write(prepared.disp_example(example_number))
+    #input_df = prepared.display_data[example_number]
 
 # 1. display recorded data from csv file table and graph
 st.header('Your bioreactor recorded data ')
+st.write("Currently using the example file selected, upload your dataset to see your results")
 if input_df is not None:
     if st.checkbox('Show dataset'):
         st.subheader('Recorded for a 24 hour period')
@@ -76,10 +81,10 @@ else:
     st.write('Awaiting CSV file to be uploaded.')
 
 # 3. Forecasting data
-with st.sidebar:
-        st.write("Select the example file you used to display its empirical data for feedstock characteristics :")
+#with st.sidebar:
+        #st.write("Select the example file you used to display its empirical data for feedstock characteristics :")
 
-        option = st.sidebar.selectbox('Results',('Example 1','Example 2','Example 3'))
+        #option = st.sidebar.selectbox('Results',('Example 1','Example 2','Example 3'))
 
 st.header('Empirical data')
 st.markdown("""
@@ -93,11 +98,11 @@ ex3_df = pd.read_csv("feedstock3.csv")
 
 
 #Displaying feedstock characteristics depending on the example file chosen by the user
-if 'Example 1' in option:
+if 'Example 1' in example:
             st.write(ex1_df)
-elif 'Example 2' in option:
+elif 'Example 2' in example:
             st.write(ex2_df)
-elif'Example 3' in option:
+elif'Example 3' in example:
             st.write(ex3_df)
 
 st.markdown("""
